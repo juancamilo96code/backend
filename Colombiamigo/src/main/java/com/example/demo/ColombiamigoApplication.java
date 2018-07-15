52f6246fb6b046d5279dacab116a11d275913db8
juancamilo96code <41132803+juancamilo96code@users.noreply.github.com>
2018-07-15 20:33:39 -0300

    Add files via upload

---
+++ Colombiamigo/src/main/java/com/example/demo/ColombiamigoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombiamigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColombiamigoApplication.class, args);
+	}
+}
